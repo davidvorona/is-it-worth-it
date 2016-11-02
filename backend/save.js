effae0e4790adc00fd9ca48375017449cf3c77c6
davidvorona <davidavorona@gmail.com>
2016-11-02 14:01:35 -0700

    fixes

--- backend/save.js
+++ backend/save.js
@@ -1,15 +1,10 @@
+// this runs scraper
 const request = require('request');
-const test = require('./movie').test;
 
 const API_URL = 'http://localhost:3000/scrape';
-const migrate = require('./mongoMethods').saveMovies;
-const clear = require('./mongoMethods').clearMovies;
 
 request(API_URL, (err, res, body) => {
-  const data = JSON.parse(body);
-  test('Connection test called in save.js...');
-  clear();
-  migrate(data);
+  return JSON.parse(body);
 });
 
 // 'npm run db reel-movies' to run `save.js` file and save scraped values to database
